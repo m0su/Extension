48a1dab9bb63459028d4896e45921e4b7248d7fb
m0su <sirm0su@gmail.com>
2021-09-24 15:07:00 +0900

    add Integer+

---
+++ Sources/Extension/Integer+.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by seo on 2021/09/24.
+//
+
+public extension IntegerLiteralType {
+  var f: CGFloat {
+    return CGFloat(self)
+  }
+  
+  var timeInterval_s: DispatchTimeInterval { return .seconds(self) }
+  var timeInterval_ms: DispatchTimeInterval { return .milliseconds(self) }
+}
+
+public extension FloatLiteralType {
+  var f: CGFloat {
+    return CGFloat(self)
+  }
+}
+
